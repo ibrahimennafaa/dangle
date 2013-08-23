920181bf1d9a396e5c16ba1705b56105d7ba0ad1
Charles Mouliard <cmoulliard@apache.org>
2013-08-23 16:15:22 +0200

    Resolves #6: Add a dangle example based on the demo

---
+++ example/js/controllers.js
@@ -0,0 +1,118 @@
+angular.module('demo.controllers', [])
+    .controller('DemoCtrl', function($scope) {
+        'use strict';
+
+        var resultsA = {
+          facets: {
+          Product : {
+              _type : "terms",
+              missing : 0,
+              total : 454,
+              other : 0,
+              terms : [{
+                term : "Prod-A",
+                count : 306
+              },{
+                term : "Prod-B",
+                count : 148
+              },{
+                term : "Prod-C",
+                count : 62
+              }]
+          },
+          Sex : {
+              _type : "terms",
+              missing : 0,
+              total : 454,
+              other : 0,
+              terms : [{
+                term : "Male",
+                count : 36
+              },{
+                term : "Female",
+                count : 148
+              }]
+          },
+            Times : {
+              _type: "date_histogram",
+              entries : [{
+                time : 1341100800000,
+                count : 9
+              }, {
+                time : 1343779200000,
+                count : 32
+              }, {
+                time : 1346457600000,
+                count : 78
+              }, {
+                time : 1349049600000,
+                count : 45
+              }, {
+                time : 1351728000000,
+                count : 134
+              }]
+            }
+          }
+        };
+
+        var resultsB = {
+          facets: {
+          Product : {
+              _type : "terms",
+              missing : 0,
+              total : 454,
+              other : 0,
+              terms : [{
+                term : "Prod-A",
+                count : 306
+              },{
+                term : "Prod-B",
+                count : 148
+              },{
+                        term : "Prod-C",
+                        count : 0
+                    }]
+          },
+          Sex : {
+              _type : "terms",
+              missing : 0,
+              total : 454,
+              other : 0,
+              terms : [{
+                term : "Male",
+                count : 36
+              }]
+          },
+            Times : {
+              _type: "date_histogram",
+              entries : [{
+                time : 1341100800000,
+                count : 9
+              }, {
+                time : 1343779200000,
+                count : 32
+              }, {
+                time : 1346457600000,
+                count : 78
+              }]
+            }
+          }
+        };
+
+        $scope.filterSearchA = function(type, term) {
+            switch(currentResults) {
+                case 'A':
+                    $scope.results = resultsB;
+                    currentResults = 'B';
+                    break;
+                case 'B':
+                    $scope.results = resultsA;
+                    currentResults = 'A';
+                    break;
+            }
+        };
+
+        $scope.results = resultsA;
+        var currentResults = 'A';
+
+    });
